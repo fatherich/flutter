ac67274f3160026370d67d896202a163ef41a06a
Irakli <iraklimosidze@gmail.com>
2021-03-20 13:28:07 +0400

    commint on master

--- brstore/lib/api_service.dart
+++ brstore/lib/api_service.dart
@@ -1,36 +1,37 @@
 import 'dart:io';
-import 'dart:ui';
 
-import 'package:brstore/models/customer.dart'
-
-import 'package:flutter/animation.dart';import 'config.dart'
+import 'package:brstore/models/customer.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'config.dart';
 
 class APIservice {
   Future<bool> createCustomer(CustomerModel model) async {
-    var authToken = base64.encode(utf8.encode(Config.key + ":" + Config.secret),
+    var authToken = base64.encode(
+      utf8.encode(Config.key + ":" + Config.secret),
     );
-    
+
     bool ret = false;
 
     try {
       var response = await Dio().post(Config.url + Config.customerURL,
-        data: model.toJson(),
-        options: new Options(headers: {
+          data: model.toJson(),
+          options: new Options(headers: {
             HttpHeaders.authorizationHeader: 'Basic $authToken',
             HttpHeaders.contentTypeHeader: "application/json"
           }));
 
-      if(response.statusCode == 201) {
+      if (response.statusCode == 201) {
         ret = true;
-      } 
-      } on Dio Error catch (e) {
-        if (e.response.statusCode == 404) {
-          ret = false;
-        } else {
-          ret = false;
-        }
       }
-      
-      return ret;
+    } on DioError catch (e) {
+      if (e.response.statusCode == 404) {
+        ret = false;
+      } else {
+        ret = false;
+      }
+    }
+
+    return ret;
   }
 }
